b8c9f72c366d68ae3724f3640a7948261c6af066
Brian Rogers <btcr1990@gmail.com>
2018-10-31 03:00:05 -0500

    Now solves famous 952 problem

--- solver.pl
+++ solver.pl
@@ -3,22 +3,20 @@ countdownsolve([H], Target, X):-H =:= Target, X = H.
 
 countdownsolve([H1,H2|T], Target, X):-
     
-    countdownsolve(T, Target, X), !;
+    countdownsolve([H1 | T], Target, X);
     
-    countdownsolve([H1 | T], Target, X), !;
-    
-    countdownsolve([H2 | T], Target, X), !;
+    countdownsolve([H2 | T], Target, X);
     
     H1 > 1, H2 > 1, Mul = H1 * H2, 
     countdownsolve([Mul | T], Target, X);
     
-    H2 =\= 0, Div = H1 / H2, integer(Div),
+    H2 =\= 0, Div = H1 / H2, H1 mod H2 =:= 0,
     countdownsolve([Div | T], Target, X);
     
     Add = H1 + H2,
     countdownsolve([Add | T], Target, X);
     
-    Sub = H1 - H2,
+    Sub = H1 - H2, Sub > 0,
     countdownsolve([Sub | T], Target, X).
 
 
