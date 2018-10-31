f50e72166bd9ecf68045a0e477e78e4c72254bfb
Brian Rogers <btcr1990@gmail.com>
2018-10-31 01:40:57 -0500

    Naive solution...maybe

---
+++ solver.pl
@@ -0,0 +1,27 @@
+countdownsolve([H], Target, X):-H =:= Target, X = H; abs(H - Target) =< 10, X = H.
+
+
+countdownsolve([H1,H2|T], Target, X):-
+    
+    countdownsolve(T, Target, X), !;
+    
+    countdownsolve([H1 | T], Target, X), !;
+    
+    countdownsolve([H2 | T], Target, X), !;
+    
+    H1 > 1, H2 > 1, Mul = H1 * H2, 
+    countdownsolve([Mul | T], Target, X);
+    
+    H2 =\= 0, Div = H1 / H2, integer(Div),
+    countdownsolve([Div | T], Target, X);
+    
+    Add = H1 + H2,
+    countdownsolve([Add | T], Target, X);
+    
+    Sub = H1 - H2,
+    countdownsolve([Sub | T], Target, X).
+
+
+countdown([H|T], Target, X):-
+    permutation([H|T], Perm),
+    countdownsolve(Perm, Target, X).
